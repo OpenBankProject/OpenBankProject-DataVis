274d05393275570a3718a35be38306158fbc6050
Benjamin Bosters <benjamin.bosters@gmail.com>
2012-06-02 17:49:09 +0200

    db schema updated again

--- assets/schema/create.sql
+++ assets/schema/create.sql
@@ -1,8 +1,3 @@
-CREATE TABLE bank (
-  bank_id INTEGER PRIMARY KEY,
-  name VARCHAR(200)
-);
-
 CREATE TABLE date (
   date_id INTEGER PRIMARY KEY,
   date TIMESTAMP
@@ -10,13 +5,13 @@ CREATE TABLE date (
 
 CREATE TABLE transaction_partner (
   tp_id INTEGER PRIMARY KEY,
-  name VARCHAR(200),
-  alias VARCHAR(200),
-  bank_id INTEGER REFERENCES bank(bank_id) NOT NULL
+  account_holder VARCHAR(200),
+  bank_name VARCHAR(200)
 );
 
 CREATE TABLE category (
   category_id INTEGER PRIMARY KEY,
+  transaction_type VARCHAR(7) CHECK (transaction_type IN ('income','expense')) NOT NULL,
   name VARCHAR(200),
   parentcategoryid INTEGER REFERENCES Category(category_id)
 );
@@ -28,6 +23,5 @@ CREATE TABLE transaction (
   category_id INTEGER REFERENCES category(category_id) NOT NULL,
   amount DOUBLE PRECISION NOT NULL,
   currency VARCHAR(100),
-  transaction_type VARCHAR(7) CHECK (type IN ('income','expense')),
-  name VARCHAR(200)
+  account_holder VARCHAR(200)
 );
